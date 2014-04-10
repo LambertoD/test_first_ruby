3d7f1bbdb203a6fbb6e2186ae2f7a9820ea0ce08
Lamberto Diwa <lambertodiwajr@gmail.com>
2014-04-09 21:41:45 -0700

    update exercise 10 with class methods

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,9 +1,55 @@
 class Temperature
-  attr_reader :temp
+  attr_accessor :c_temp, :f_temp
+  @@temp = 0
+  @@degree = {}
   def initialize(args = {})
-    @temp = args
+    @f_temp = args if args[:f]
+    @c_temp = args if args[:c]
   end
   def in_fahrenheit
-    puts @temp.keys
+    temp = if @f_temp
+      @f_temp[:f]
+    else
+      @c_temp[:c] * 1.8 + 32
+    end
+  end
+  def in_celsius
+    temp = if @f_temp
+      ((@f_temp[:f] - 32) / 1.8).ceil
+    else
+      @c_temp[:c]
+    end
+  end
+  def self.from_celsius(number)
+    @@temp = number
+    @@degree[:c] = true
+    @@degree[:f] = nil
+    self
+  end
+  def self.in_celsius
+    if @@degree[:c]
+      @@temp
+    else
+      ((@@temp - 32) / 1.8).ceil
+    end
+  end
+  def self.in_fahrenheit
+    if @@degree[:f]
+      @@temp
+    else
+      @@temp * 1.8 + 32 if @@temp
+    end
+  end
+  def self.from_fahrenheit(number)
+    @@temp = number
+    @@degree[:f] = true
+    @@degree[:c] = nil
+    self
+  end
+end
+class Celsius < Temperature
+  attr_accessor :temp
+  def initialize(number)
+    @temp = number
   end
 end
