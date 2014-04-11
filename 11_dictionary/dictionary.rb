6ae8aa04e547a2d3570ecb094cbc038a21de4971
Lamberto Diwa <lambertodiwajr@gmail.com>
2014-04-10 22:47:45 -0700

    update dictionary to pass more tests

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -1,12 +1,33 @@
 class Dictionary
   attr_accessor :entries
   def initialize
-    @entries = {}
+    @entries = Hash.new(nil)
   end
   def add(value)
-    @entries = value
+    if value.is_a? Hash
+      @entries.merge! value 
+    else
+      @entries = {"#{value}" => nil} 
+    end
   end
   def keywords
     @entries.keys
   end
+  def include?(key)
+    @entries.has_key?(key)
+  end
+  def find(value)
+    if @entries.empty?
+      {}
+    elsif include?(value)
+      puts "got elsif  with #{value} and #{@entries}"
+      @entries
+    else
+      puts "got else  with #{value} and #{@entries}"
+      stuff = @entries.find_all {|key,value| key.start_with? value}
+      puts stuff
+      stuff
+    end
+  end
+
 end
