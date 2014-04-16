4693d2097873d433fc9c714d31de870bd79c0324
Lamberto Diwa <lambertodiwajr@gmail.com>
2014-04-15 21:27:27 -0700

    complete all in_word translations

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -10,8 +10,10 @@ class Fixnum
       num_string = get_thousand_digits(number)
     elsif thousand < 999999
       num_string = get_millions_digits(number)
-    else
+    elsif thousand < 999999999
       num_string = get_billions_digits(number)
+    else
+      num_string = get_trillions_digits(number)
     end
     num_string
   end
@@ -73,23 +75,43 @@ class Fixnum
   def get_millions_digits(number)
     million  = number/1000000
     thousand = number%1000000
-    million_string = extract_digits(million) + " million"
+    million_string = ""
+    million_string = extract_digits(million) + " million" if million > 0
     thousand_string = ""
     if thousand > 999
       thousand_string = " " + get_thousand_digits(thousand) 
     else
       thousand_string = get_thousand_digits(thousand)
     end
-    
+
     million_string + thousand_string
   end
   def get_billions_digits(number)
     billion = number/1000000000
     million = number%1000000000
-    billion_string = extract_digits(billion) + " billion"
+    billion_string = ""
+    billion_string = extract_digits(billion) + " billion" if billion > 0
     million_string = ""
-    million_string = " " + get_millions_digits(million) if billion > 0
+    if million > 999
+      million_string = " " + get_millions_digits(million) 
+    else
+      million_string = get_millions_digits(million)
+    end
     billion_string + million_string
+  end
+  def get_trillions_digits(number)
+
+    trillion = number/1000000000000
+    billion  = number%1000000000000
+    trillion_string = ""
+    trillion_string = extract_digits(trillion) + " trillion"
+    billion_string = ""
+    if billion > 999
+      billion_string = " " + get_billions_digits(billion) 
+    else
+      billion_string = get_billions_digits(billion)
+    end
+    trillion_string + billion_string
   end
 end
 
